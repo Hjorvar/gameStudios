108130c2545b06aa3e7a7ec4f2be71fb230176c0
Hjorvar <hih@fb.is>
2021-09-15 07:43:53 +0000

    breyting á DB

--- db/scripts.sql
+++ db/scripts.sql
@@ -1,4 +1,11 @@
 CREATE TABLE studios (
   id INTEGER PRIMARY KEY,
-  name TEXT NOT NULL UNIQUE
-  );
+  name TEXT NOT NULL UNIQUE,
+  staffAmmount INT,
+  city TEXT,
+  country TEXT
+);
+
+DROP TABLE studios;
+
+SELECT * FROM studios;
