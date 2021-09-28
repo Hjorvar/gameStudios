6be09e9d7f89854f536b8f4803d080d370d52d46
Hjorvar <hih@fb.is>
2021-09-28 12:52:11 +0000

    update virkar núna alveg

--- routes/update/studio.js
+++ routes/update/studio.js
@@ -6,6 +6,37 @@ const colors = require('colors');
 
 const router = express.Router();
 
+
+function updateStudio(dbFile ,id, name, staffAmmount, city, country, founded){
+  const studio = [name, staffAmmount, city, country, founded, id];
+  const sql = 'UPDATE studios SET name = ?, staffAmmount = ?, city = ?, country = ?, founded = ?  WHERE id = ?';
+  console.log(studio);
+
+  const db = new sqlite3.Database(dbFile, (err) => {
+    if (err) {
+      return console.error(colors.red(err.message));
+    }
+    console.log('Connected to the SQLite database'.green);
+    return true;
+  });
+
+  db.run(sql, studio, (err) => {
+    if (err) {
+      return console.log(colors.red(err.message));
+    }
+    console.log(`Row updated with new info: ${studio}`.green);
+    return true;
+  });
+
+  db.close((err) => {
+    if (err) {
+      return console.error(colors.red(err.message));
+    }
+    console.log('Close the database connection'.green);
+    return true;
+  });
+}
+
 // get studioTemplate page
 router.get('/', (req, res) => {
   const idStudio = req.query.idStudio;
@@ -37,8 +68,8 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  db.updateStudio(1, req.body.studioName);
-  res.json(req.body);
+  updateStudio(dbFile, req.body.idStudio, req.body.studioName, req.body.staffAmmount, req.body.city, req.body.country, req.body.founded );
+  res.redirect('/studios');
 });
 
 module.exports = router;
