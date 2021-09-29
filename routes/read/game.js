c51ed7dcf9b298db56c61ff5915bb95f00ecf832
Hjorvar <hih@fb.is>
2021-09-29 13:19:21 +0000

    read/game komið með better-sqlite

--- routes/read/game.js
+++ routes/read/game.js
@@ -1,45 +1,14 @@
 const express = require('express');
 const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
-const colors = require('colors');
-
+const readGame = require('../../db/readGame');
 const dbFile = path.join(__dirname, '../../db/gameStudios.db');
 
 const router = express.Router();
 
 // get index page
 router.get('/', (req, res) => {
-	const sql = 'SELECT games.id, games.name AS name, studios.name AS studioName, GROUP_CONCAT(genres.name) AS genresName, games.youtubeTrailer AS trailer, games.info AS info FROM games INNER JOIN gameGenres ON games.id = gameGenres.idGame INNER JOIN studios ON games.idStudio = studios.id INNER JOIN genres ON gameGenres.idGenre = genres.id WHERE games.id = ? GROUP BY games.id ORDER BY games.name;';
-  let game = "";
-	let gameId = req.query.idGame;
-
-  const db = new sqlite3.Database(dbFile, (err) => {
-    if (err) {
-      return console.error(colors.red(err.message));
-    }
-    console.log('Connected to the SQLite database'.green);
-    return true;
-  });
-
-  db.get(sql, [gameId], (err, row) => {
-    if (err) {
-      return console.log(colors.red(err.message));
-    }
-    console.log('Reading data from table'.green);
-    game = row;
-  
-    // colors.yellow(console.log(studios));
-    res.render('read/game', { title: 'Game', game });
-    return true;
-  });
-
-  db.close((err) => {
-    if (err) {
-      return console.error(colors.red(err.message));
-    }
-    console.log('Close the database connection'.green);
-    return true;
-  });
+  const game = readGame(dbFile, req.query.idGame);
+  res.render('read/game', { title: 'Game', game });
 
 
 });
--- routes/read/studios.js
+++ routes/read/studios.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-// const sqlite3 = require('sqlite3').verbose();
 const readStudios = require('../../db/readStudios');
 const dbFile = path.join(__dirname, '../../db/gameStudios.db');
 
@@ -15,41 +14,6 @@ router.get('/', (req, res) => {
 
   const studios = readStudios(dbFile);
   res.render('read/studios', { title: 'Studios', studios, username });
-
-
-  // const sql = 'SELECT id, name, country, city, staffAmmount FROM studios ORDER BY name';
-  // let studios = [];
-
-  // const db = new sqlite3.Database(dbFile, (err) => {
-  //   if (err) {
-  //     return console.error(colors.red(err.message));
-  //   }
-  //   console.log('Connected to the SQLite database'.green);
-  //   return true;
-  // });
-
-  // db.all(sql, [], (err, rows) => {
-  //   if (err) {
-  //     return console.log(colors.red(err.message));
-  //   }
-  //   console.log('Reading data from table'.green);
-  //   rows.forEach((row) => {
-  //     studios.push(row);
-  //   })
-
-  //   res.render('read/studios', { title: 'Studios', studios, username });
-  //   return true;
-  // });
-
-  // db.close((err) => {
-  //   if (err) {
-  //     return console.error(colors.red(err.message));
-  //   }
-  //   console.log('Close the database connection'.green);
-  //   return true;
-  // });
-
-  // res.render('read/studios', { title: 'Studios', studios, username });
 
 });
 
