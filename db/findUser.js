6472e568258ae4cd7ed76a5803f6f109d4a594be
Brynjar Haraldsson <brynjar04@gmail.com>
2021-09-29 19:22:51 +0000

    Login is now no longer case sensitive

--- db/findUser.js
+++ db/findUser.js
@@ -2,7 +2,7 @@ const Database = require('better-sqlite3');
 
 module.exports = function readStudios(dbFile, user, password) {
   const db = new Database(dbFile, { verbose: console.log });
-  const stmt = db.prepare('SELECT username FROM users WHERE username = ? AND password = ?');
+  const stmt = db.prepare('SELECT username FROM users WHERE username LIKE ? AND password = ?');
   const studios = stmt.get(user, password);
   db.close();
   return studios
