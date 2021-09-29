3eeb5fe8d3e45bf4bb685c96c97dd6250ea0ddde
Hjorvar <hih@fb.is>
2021-09-29 15:32:03 +0000

    smá breytingar

--- routes/update/genre.js
+++ routes/update/genre.js
@@ -2,9 +2,7 @@ const express = require('express');
 const path = require('path');
 const dbFile = path.join(__dirname, '../../db/gameStudios.db');
 
-
 const router = express.Router();
-
 
 // get studioTemplate page
 router.get('/', (req, res) => {
