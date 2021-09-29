1225265542553ddb0de93198052ed0b1677efd3a
Hjorvar <hih@fb.is>
2021-09-29 14:37:10 +0000

    login.js komið með better-sqlite

---
+++ db/findUser.js
@@ -0,0 +1,12 @@
+const Database = require('better-sqlite3');
+
+module.exports = function readStudios(dbFile, user, password) {
+  const db = new Database(dbFile, { verbose: console.log });
+  const stmt = db.prepare('SELECT username FROM users WHERE username = ? AND password = ?');
+  const studios = stmt.get(user, password);
+  db.close();
+  return studios
+}
+
+
+
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
+const findUser = require('../db/findUser');
 const dbFile = path.join(__dirname, '../db/gameStudios.db');
-const colors = require('colors');
 
 const router = express.Router();
 
@@ -14,39 +13,16 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
 
-  const user = [req.body.user, req.body.password];
-  const sql = 'SELECT username FROM users WHERE username = ? AND password = ?;';
-  const db = new sqlite3.Database(dbFile, (err) => {
-    if (err) {
-      return console.error(colors.red(err.message));
-    }
-    console.log('Connected to the SQLite database'.green);
-    return true;
-  });
-
-  db.get(sql, user, (err, row) => {
-    if (err) {
-      return console.log(colors.red(err.message));
-    }
-    console.log('Reading data from table'.green);
-    if(row){
-      req.session.loggedin = true;
-      req.session.username = row.username;
-      res.redirect('/');
-    } else {
-      res.send('Incorrect Username and/or Password!');
-    }
-  
-    return true;
-  });;
-
-  db.close((err) => {
-    if (err) {
-      return console.error(colors.red(err.message));
-    }
-    console.log('Close the database connection'.green);
-    return true;
-  });
+  // const user = [req.body.user, req.body.password];
+  const user = findUser(dbFile, req.body.user, req.body.password)
+  console.log(user)
+  if(user){
+    req.session.loggedin = true;
+    req.session.username = user.username;
+    res.redirect('/');
+  } else {
+    res.send('Incorrect Username and/or Password!');
+  }
 });
 
 module.exports = router;
