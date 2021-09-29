c161389c75b6365530429bf62eefc27c49a47088
Hjorvar <hih@fb.is>
2021-09-29 14:55:13 +0000

    updateStudio fallið komið í sérskjal og í better sqlite form

---
+++ db/updateStudio.js
@@ -0,0 +1,9 @@
+const Database = require('better-sqlite3');
+
+module.exports = function readStudios(dbFile, id, name, staffAmmount, city, country, founded) {
+  const db = new Database(dbFile, { verbose: console.log });
+  const stmt = db.prepare('UPDATE studios SET name = ?, staffAmmount = ?, city = ?, country = ?, founded = ?  WHERE id = ?');
+  const studios = stmt.run(name, staffAmmount, city, country, founded, id);
+  db.close();
+  return studios
+}
--- routes/update/studio.js
+++ routes/update/studio.js
@@ -1,42 +1,10 @@
 const express = require('express');
 const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
 const dbFile = path.join(__dirname, '../../db/gameStudios.db');
-const colors = require('colors');
 const findStudio = require('../../db/findStudio');
+const updateStudio = require('../../db/updateStudio');
 
 const router = express.Router();
-
-
-function updateStudio(dbFile, id, name, staffAmmount, city, country, founded){
-  const studio = [name, staffAmmount, city, country, founded, id];
-  const sql = 'UPDATE studios SET name = ?, staffAmmount = ?, city = ?, country = ?, founded = ?  WHERE id = ?';
-  console.log(studio);
-
-  const db = new sqlite3.Database(dbFile, (err) => {
-    if (err) {
-      return console.error(colors.red(err.message));
-    }
-    console.log('Connected to the SQLite database'.green);
-    return true;
-  });
-
-  db.run(sql, studio, (err) => {
-    if (err) {
-      return console.log(colors.red(err.message));
-    }
-    console.log(`Row updated with new info: ${studio}`.green);
-    return true;
-  });
-
-  db.close((err) => {
-    if (err) {
-      return console.error(colors.red(err.message));
-    }
-    console.log('Close the database connection'.green);
-    return true;
-  });
-}
 
 // get studioTemplate page
 router.get('/', (req, res) => {
