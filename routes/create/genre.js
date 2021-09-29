78569f0cc96316f731073962ffaa1f2ce17d9105
Hjorvar <hih@fb.is>
2021-09-29 15:05:42 +0000

    createGenre komið með better sqlite

--- routes/create/genre.js
+++ routes/create/genre.js
@@ -1,38 +1,9 @@
 const express = require('express');
 const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
+const createGenre = require('../../db/createGenre');
 const dbFile = path.join(__dirname, '../../db/gameStudios.db');
-const colors = require('colors');
 
 const router = express.Router();
-
-function createGenre(dbFile, name){
-  const studio = [name];
-  const sql = 'INSERT INTO genres(name) VALUES (?)';
-  const db = new sqlite3.Database(dbFile, (err) => {
-    if (err) {
-      return console.error(colors.red(err.message));
-    }
-    console.log('Connected to the SQLite database'.green);
-    return true;
-  });
-
-  db.run(sql, studio, (err) => {
-    if (err) {
-      return console.log(colors.red(err.message));
-    }
-    console.log(colors.green(studio) + ' added to DB');
-    return true;
-  });
-
-  db.close((err) => {
-    if (err) {
-      return console.error(colors.red(err.message));
-    }
-    console.log('Close the database connection'.green);
-    return true;
-  });
-}
 
 // get studioTemplate page
 router.get('/', (req, res) => {
@@ -41,12 +12,10 @@ router.get('/', (req, res) => {
 	} else {
     console.log('einhver reyndi að koma hingað sem má það ekki'.red);
     res.redirect(301, '/' );
-
 	}
 });
 
 router.post('/', (req, res) => {
-  console.log('pabbi');
   if (req.session.loggedin) {
     createGenre(dbFile, req.body.genreName);
     res.render('createUpdate/genres', { title: 'Create', action: 'create' });
@@ -54,7 +23,6 @@ router.post('/', (req, res) => {
     console.log('einhver reyndi að koma hingað sem má það ekki'.red);
     res.redirect(301, '/' );
 	}
-
 });
 
 module.exports = router;
