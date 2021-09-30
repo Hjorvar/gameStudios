30f6d256d526c9fb55ee4b4aab0ae078f4ffbf71
Brynjar Haraldsson <brynjar04@gmail.com>
2021-09-30 21:47:34 +0000

    shortened long line

--- db/read/readGame.js
+++ db/read/readGame.js
@@ -2,7 +2,15 @@ const Database = require('better-sqlite3');
 
 module.exports = function readStudios(dbFile, idGame) {
   const db = new Database(dbFile, { verbose: console.log });
-  const stmt = db.prepare('SELECT games.id, games.name AS name, studios.name AS studioName, GROUP_CONCAT(genres.name) AS genresName, games.youtubeTrailer AS trailer, games.info AS info FROM games INNER JOIN gameGenres ON games.id = gameGenres.idGame INNER JOIN studios ON games.idStudio = studios.id INNER JOIN genres ON gameGenres.idGenre = genres.id WHERE games.id = ? GROUP BY games.id ORDER BY games.name;');
+  const stmt = db.prepare(`
+  SELECT games.id, games.name AS name, studios.name AS studioName,
+  GROUP_CONCAT(genres.name) AS genresName,
+  games.youtubeTrailer AS trailer, games.info AS info
+  FROM games 
+  INNER JOIN gameGenres ON games.id = gameGenres.idGame 
+  INNER JOIN studios ON games.idStudio = studios.id 
+  INNER JOIN genres ON gameGenres.idGenre = genres.id 
+  WHERE games.id = ? GROUP BY games.id ORDER BY games.name;`);
   const game = stmt.get(idGame);
   db.close();
   return game
