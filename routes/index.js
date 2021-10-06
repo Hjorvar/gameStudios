83495dcb99a721c5182d23620bdb08cce8bc97f6
Brynjar Haraldsson <brynjar04@gmail.com>
2021-10-06 14:52:56 +0000

    Front page now shows the games coming out this month or the next

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,34 @@
 const express = require('express');
+const path = require('path');
+const readGames = require('../db/read/readGames');
+
+const dbFile = path.join(__dirname, '../db/gameStudios.db');
 
 const router = express.Router();
 
 // get index page
 router.get('/', (req, res) => {
+  const currentDate = new Date;
+  let where = 'WHERE month IN (?, ?) AND year = ?';
+  let search = []
+  search.push(currentDate.getMonth() + 1);
+  if (currentDate.getMonth() + 1 == 12) {
+    // So the next month isn't 13
+    // Note: getMonth starts on 0
+    search.push(1);
+    where += ' OR year = ?';
+    search.push(currentDate.getFullYear());
+    search.push(currentDate.getFullYear() + 1);
+  } else {
+    search.push(currentDate.getMonth() + 2);
+    search.push(currentDate.getFullYear());
+  }
+  const games = readGames(dbFile, where, search);
+  let username = 'none';
   if (req.session.loggedin) {
-    const username = req.session.username;
-    res.render('index', { title: 'Front page', username });
-	} else {
-    const username = 'none';
-    res.render('index', { title: 'Front page', username });
+    username = req.session.username;
 	}
+  res.render('index', { title: 'Front page', username, games });
 });
 
 module.exports = router;
