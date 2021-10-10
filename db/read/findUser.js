21c40604bc61923e4b4f8ce9e5bd1b5695cf47eb
Hjorvar <hih@fb.is>
2021-10-10 11:00:22 +0000

    lagaði findUser skjalið, það var með röngum nöfnum

--- db/read/findUser.js
+++ db/read/findUser.js
@@ -1,11 +1,11 @@
 const Database = require('better-sqlite3');
 
-module.exports = function readStudios(dbFile, user, password) {
+module.exports = function findUser(dbFile, user, password) {
   const db = new Database(dbFile);
   const stmt = db.prepare('SELECT username FROM users WHERE username = ? COLLATE NOCASE AND password = ?');
-  const studios = stmt.get(user, password);
+  const user = stmt.get(user, password);
   db.close();
-  return studios
+  return user
 }
 
 
