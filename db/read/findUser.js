48e5600352eeca3af5c3941ae685631a99a3a3d7
Hjorvar <hih@fb.is>
2021-10-10 13:26:28 +0000

    lagfæring á findeUser bjó til litla villu, vonandi lagað núna

--- db/read/findUser.js
+++ db/read/findUser.js
@@ -1,9 +1,9 @@
 const Database = require('better-sqlite3');
 
-module.exports = function findUser(dbFile, user, password) {
+module.exports = function findUser(dbFile, username, password) {
   const db = new Database(dbFile);
   const stmt = db.prepare('SELECT username FROM users WHERE username = ? COLLATE NOCASE AND password = ?');
-  const user = stmt.get(user, password);
+  const user = stmt.get(username, password);
   db.close();
   return user
 }
